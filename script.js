217b5012b48409a197ff1e81b08e95e345b8cccd
Suraj <suraj09888888@gmail.com>
2026-07-18 14:18:51 +0530

    js code

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector(".menu");
+let navLinks = document.querySelector(".nav-top-middle");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
