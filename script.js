ecc455976ee8ec162039197e7a2af96a452fab96
Suraj <suraj09888888@gmail.com>
2026-07-24 16:17:29 +0530

    gallery js added

--- script.js
+++ script.js
@@ -5,4 +5,25 @@ menu.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+let buttons = document.querySelectorAll(".filter-btn");
+let images = document.querySelectorAll(".gallery-item");
 
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    let category = button.dataset.filter;
+
+    buttons.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+
+    images.forEach(function (image) {
+      if (category === "all" || image.classList.contains(category)) {
+        image.style.display = "block";
+      } else {
+        image.style.display = "none";
+      }
+    });
+  });
+});
