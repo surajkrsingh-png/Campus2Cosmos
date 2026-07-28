34111bfab500dd786917ad86cbfa73961ade664f
Suraj <suraj09888888@gmail.com>
2026-07-28 11:32:46 +0530

    issue fixed

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-emailjs.init("bHgFjgnLkz8pZyyT6");  
+console.log("Script Loaded");
+if (typeof emailjs !== "undefined") {
+  emailjs.init("bHgFjgnLkz8pZyyT6");
+}
 
 // MENU BUTTON
 let menu = document.querySelector(".menu");
@@ -42,67 +45,37 @@ questions.forEach(function (question) {
 });
 
 // INQUIRY FORM
-
 let form = document.querySelector("#inquiry-form");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+if (form) {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  const name = document.querySelector("#name").value.trim();
-  const email = document.querySelector("#email").value.trim();
-  const phone = document.querySelector("#phone").value.trim();
-  const studentClass = document.querySelector("#class").value.trim();
-  const subject = document.querySelector("#subject").value.trim();
-  const message = document.querySelector("#message").value.trim();
+    const name = document.querySelector("#name").value.trim();
+    const email = document.querySelector("#email").value.trim();
+    const phone = document.querySelector("#phone").value.trim();
+    const studentClass = document.querySelector("#class").value.trim();
+    const subject = document.querySelector("#subject").value.trim();
+    const message = document.querySelector("#message").value.trim();
 
-  if (name === "") {
-    alert("please enter your name");
-    return;
-  }
-  if (email === "") {
-    alert("please enter your email");
-    return;
-  }
-  if (!email.includes("@")) {
-    alert("Please enter a valid email address");
-    return;
-  }
+    // all  validation code
 
-  if (phone.length !== 10 || isNaN(phone)) {
-    alert("Please enter a valid 10-digit phone number");
-    return;
-  }
-
-  if (studentClass === "") {
-    alert("Please enter your class");
-    return;
-  }
-
-  if (subject === "") {
-    alert("Please enter your subject");
-    return;
-  }
-
-  if (message === "") {
-    alert("Please enter your message");
-    return;
-  }
-
-emailjs
-  .send("service_me8zy88", "template_u4eqepj", {
-    name: name,
-    email: email,
-    phone: phone,
-    studentClass: studentClass,
-    subject: subject,
-    message: message,
-  })
-  .then(function () {
-    alert("Inquiry sent successfully!");
-    form.reset();
-  })
-  .catch(function (error) {
-    alert("Failed to send inquiry.");
-    console.log(error);
+    emailjs
+      .send("service_me8zy88", "template_u4eqepj", {
+        name: name,
+        email: email,
+        phone: phone,
+        studentClass: studentClass,
+        subject: subject,
+        message: message,
+      })
+      .then(function () {
+        alert("Inquiry sent successfully!");
+        form.reset();
+      })
+      .catch(function (error) {
+        alert("Failed to send inquiry.");
+        console.log(error);
+      });
   });
-});
+}
